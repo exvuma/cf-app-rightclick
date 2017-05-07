b36f4d5e54df6c019026ed04ab7f9b84f37d49da
Victoria Bernard <victoria@cloudflare.com>
2017-05-07 06:41:49 -0500

    Highlight, italic work

--- right-click.js
+++ right-click.js
@@ -10,8 +10,8 @@
   var formats={} //dict of formats that are enabled
   function updateOpts(){
       Right_Click.options = options
-       for (var key in options){// temporarily set formats var TODO change to when button is enabeld
-            if( Right_Click.options[key]){
+       for (var key in options.modifiers){// temporarily set formats var TODO change to when button is enabeld
+            if( Right_Click.options.modifiers[key]){
             formats[key] = true
           }
           else {
@@ -29,10 +29,10 @@
       var options = {
                 "backgroundColor": "#FFFFFF",
                 "modifiers": {
-                  "bold": true,
-                  "highlight": true,
-                  "format_italic": true,
-                  "code": true
+                  "cf-bold": true,
+                  "cf-highlight": true,
+                  "cf-italic": true,
+                  "cf-code": false
                 }
               }
     for (var key in options)
@@ -55,45 +55,176 @@
  // for (var type in Right_Click.options){
    var buttons = document.getElementsByClassName("btn btn-info")
    for (var b in buttons){
-     console.log(buttons[b])
-     if (buttons[b].state == focus){
-       console.log(buttons[b].id)
+    //  console.log(buttons[b])
+     if(!buttons[b])
+      continue
+     if (buttons[b].getAttribute("active") == "true"){
+       formats[buttons[b].firstChild.id] = true
+       console.log("this button is in focus" + buttons[b].firstChild.id)
      }
+     else
+        formats[buttons[b].firstChild.id] = false
+      }
+   return formats
  }
+ function getFormatString(){
+   var final = ""
+   for (var key in formats){
+    //  console.log(buttons[b])
+     if (formats[key]){
+       if(final)
+          final += " " + key
+        else
+        final = key
+     }
 
+   }
+  return final
  }
    //create and set and array that holds the formats we wish to set
  function toggleFormat(format){
    for (var type in Right_Click.options){
-     if(!formats[key]){
-        formats[key] = true
-      }
+        formats[key] = !formats[key]
      }
    }
+  function removeEl(element){
+    /// for example turn <span> some html </span> into simply some html
+    console.log("befpre" + element.parentNode.childNodes)
+    var string = element.innerHTML
+    var replaceNode = document.createTextNode(string)
+    var par = element.parentNode
+    var tempParent =  element.parentNode
+    par.replaceChild(replaceNode, element)
+    document.replaceChild(par,tempParent)
+    console.log("after" )
 
-function modifySelection(modifyPos, e){
+  }
+function checkforSpan(node, format){
+  var newParentNode = document.createElement("SPAN")
+
+  var child;
+  if(node.nodeType == 3)
+    return node
+  else if (node.hasChildNodes()){
+    //check that child don't have span or children with span recursively
+    var numKids = node.childNodes.length
+    for(var i = 0 ; i < numKids; i++){
+      child =  node.childNodes[i]
+      newParentNode.appendChild(checkforSpan(child))
+     }}
+    // Remove this format from list
+
+    // Remove Tag and return either one/two TEXT's node and a element or a text node
+  if(node.tagName == "SPAN" && (node.classList =="" || node.classList ==format)){
+    if (!node.hasChildNodes()){
+      var newTextNode = document.createTextNode(node.textContent)
+      return newTextNode
+    }
+  }
+  // remove format if it is already there in class List
+  else if(node.classList.contains(format)){
+    node.classList.remove(format)
+  }
+
+}
+function turnOnFormat(sel, format){
+  var range = sel.getRangeAt(0)
+  var contents = range.extractContents()
+  var tempDoc = document.createDocumentFragment()
+  //if selection Text Node add span
+
+  // if hasChildren, check that children don't already have an other empty span classList
+  var numKids = contents.childNodes.length
+  for(var i = 0 ; i < numKids; i++){
+    var goodNode = checkforSpan(contents.childNodes[i], format)
+    tempDoc.appendChild(goodNode)
+
+  }
+}
+function turnOffFormat(sel, format){
+  var range = sel.getRangeAt(0)
+  var contents = range.extractContents()
+  var tempDoc = document.createDocumentFragment()
+  //if selection Text Node add span
+  if(node.tagName == "SPAN" && node.classList =="")
+    return newTextNode
+  // if hasChildren, check that children don't already have an other empty span classList
+  var numKids = contents.childNodes.length
+  for(var i = 0 ; i < numKids; i++){
+    var goodNode = checkforSpan(contents.childNodes[i], format)
+    tempDoc.appendChild(goodNode)
+  }
+  // if()
+}
+/// Toggles formatClass on and off of the <span> tags involving the selection passed in as sel
+function modifySelection(sel, formatClass){
   if (!currSelection){
     if (!SelectEvent) return
     currSelection = SelectEvent.getSelection()
   }
+  var anchor = currSelection.anchorNode
+  var parEl = anchor.parentElement
+  // if <span >INNER TEXT</span> then convert to simple TEXT Node
+  if(parEl == currSelection.focusNode.parentElement && parEl.tagName == "SPAN" && parEl.classList == ""){
+        removeEl(parEl)
+  }
+
   // Check if the item has already been bolded then make it whole selection unbold
 // 3 means this is a Node of type TEXT
-  if (currSelection.anchorNode.nodeType == 3 && currSelection.anchorNode.parentElement.tagName== "STRONG"){
-    //remove the tag by just using the innerHTML
-    currSelection.anchorNode.parentElement.outerHTML = currSelection.anchorNode.parentElement.innerHTML
-    return;
-  }// if ATTRIBUTE Node Type
-  if (currSelection.anchorNode.nodeType == 2 && currSelection.anchorNode.parentElement.tagName== "STRONG"){
-    //remove the tag by just using the innerHTML
-    currSelection.anchorNode.parentElement.outerHTML = currSelection.anchorNode.parentElement.innerHTML
-    return;
-  }//if Element type
-  if (currSelection.anchorNode.nodeType == 1 && currSelection.anchorNode.parentElement.tagName== "STRONG"){}
-  //TODO: currSelection.anchorNode.getElementsByTagName("strong")
   if (currSelection.getRangeAt(0).toString() == "")
     return
-  currSelection.baseNode.parentElement.innerHTML = currSelection.baseNode.parentElement.innerHTML.replace(currSelection.getRangeAt(0).toString(), `<strong>${currSelection.getRangeAt(0).toString()}</strong>`)
+// 3 means this is a Node of type TEXT
+  if (anchor.nodeType == 3 && parEl.classList.contains(formatClass)){
+    parEl.classList.remove(formatClass)
+    if(parEl.classList == ""&& parEl.tagName == "SPAN"){
+        //remove <span> so we don't generate a ton of those tags
+        // parEl.innerHTML = parEl.innerText
+        removeEl(parEl)
+      }
+    return;
+  }// if ATTRIBUTE Node Type, remove the entire TAG list for the parentElement ? think this means this is the beginning of an element
+  if (anchor.nodeType == 2 && parEl.classList.contains(formatClass)){
+    parEl.classList.remove(formatClass)
+    currSelection.baseNode.parentElement.innerHTML = currSelection.baseNode.parentElement.innerText
+  }//if Element type
+  if (anchor.nodeType == 1 && parEl.classList.contains(formatClass)){
+    var sis =  anchor
+    while(sis && currSelection.containsNode(sis)){
+      if(sis.nodeType == 1)
+        console.log("~~! Using sis")
+        modifySelection(sis,  formatClass)//TODO change params
+      sis.classList.remove(formatClass)
+      sis =  sis.nextSibling
+    }
+  }
+  //TODO: anchor.getElementsByTagName("strong")
+  if (currSelection.getRangeAt(0).toString() == "")
+    return
 
+  // If wasn't bolded yet then bold it!
+  // if TEXT node then extract the conents into a docFragment object then create a new node to insert at the front of that range
+  if (anchor.nodeType == 3 ){
+
+    var range = currSelection.getRangeAt(0)
+    var contents = range.extractContents()
+    var newNode ;
+    if(parEl.tagName != "SPAN" ){
+       newNode = document.createElement("SPAN")
+       newNode.classList.add(formatClass)
+       newNode.appendChild(contents)
+       range.insertNode(newNode)
+     }
+    else if (currSelection.toString() == "" || currSelection.toString() == "\n" ) {
+        console.log("whie space"+currSelection.toString())
+       return
+    }
+    else{
+      console.log("no whie space"+currSelection.toString())
+    }
+  }
+}
+
+//  currSelection.anchorNode.parentElement.classList.add("cf-selection","bold-class")
 
   // //If wanted to create a DIV to replace html..
   // var start = currSelection.anchorOffset
@@ -101,16 +232,24 @@ function modifySelection(modifyPos, e){
   // var orig = currSelection.baseNode.parentElement.innerHTML
   // var alterText = currSelection.baseNode.parentElement.innerHTML.slice(start,end)
   // var newEl = document.createElement('div')
-  // newEl.className = "bold-class"
+  // newEl.className = "cf-bold-class"
   // newEl.innerHTML = alterText
   // // currSelection.baseNode.parentElement.appendChild(newEl)
   // if(!  currSelection.baseNode.parentElement)return
   // currSelection.baseNode.parentElement.innerHTML = currSelection.baseNode.parentElement.innerHTML.replace(currSelection.getRangeAt(0).toString(), newEl.outerHTML)
   // currSelection.baseNode.parentElement.innerHTML = currSelection.baseNode.parentElement.innerHTML.replace(currSelection.getRangeAt(0).toString(), `<strong>${currSelection.getRangeAt(0).toString()}</strong>`)
-  //  currSelection.baseNode.parentElement.setAttribute("class", "bold-class")
-}
+  //  currSelection.baseNode.parentElement.setAttribute("class", "cf-bold-class")
+
  function menuClicked(){
    currSelection = window.getSelection()
+   //this is the button
+   if (this.getAttribute("active") == "true")
+      this.setAttribute("active", "false")
+   else
+      this.setAttribute("active", "true")
+   formats[this.firstChild.id] = !formats[this.firstChild.id]
+   setFormats()
+   console.log("menu click")
  }
 
   var currMenu ;
@@ -131,9 +270,10 @@ function modifySelection(modifyPos, e){
   function dbClick(){
     currMenu.close();
     currMenu = undefined
+    console.log("double click")
   }
-  function clickFunc(e) {
-    setFormats()
+  function clickFunc(e){
+  //  setFormats()
     if(window.getSelection().type == "Range"){ //if there's no menu displayed and we selected a range
       isSelect = true
       SelectEvent = e
@@ -143,13 +283,19 @@ function modifySelection(modifyPos, e){
     }
 
     // Menu is already displayed
-    if ( currMenu){
+    if (currMenu){
       var menuItems = document.getElementsByClassName("btn btn-info");
       //get the item on the menu
       //if user clicked on the menu, get that item
       var pos = currMenu.getPosition(e)
       var modifyPos = document.elementFromPoint(pos.x , pos.y);
-      modifySelection(modifyPos, e)
+      var sel = currSelection
+      var formatString =  getFormatString().split(" ")
+      console.log(formatString)
+      for(var it in formatString){
+            // turnOnFormat(sel, formats)
+            modifySelection(sel, formatString[it])
+      }
 
     }
   }
@@ -176,10 +322,10 @@ function modifySelection(modifyPos, e){
   })
 
 var iconDict = {
-  "bold": "format_bold",
-  "highlight": "highlight",
-  "format_italic": "format_italic",
-  "code": "code"
+  "cf-bold": "format_bold",
+  "cf-highlight": "highlight",
+  "cf-italic": "format_italic",
+  "cf-code": "code"
 }
 function Menu() {
   //hold DOM element.
@@ -189,7 +335,6 @@ function Menu() {
   this.el.style.position = 'absolute'
   this.el.className = "cf-menu";
   this.el.style.backgroundColor = Right_Click.options.backgroundColor
-  console.log("bacl" + Right_Click.options.backgroundColor)
 
   //Push all enabled options into arr
   var arr = []
@@ -220,10 +365,6 @@ function Menu() {
     button.appendChild(icon)
     button.addEventListener('click', menuClicked)
     buttonBar.appendChild(button)
-
-
-    // menuHTMLString += `<button  onclick="` + boldSelection() + `" type="button" class="btn btn-info" ><i class="material-icons" id = "`+ arr[item] +`">` + iconDict[arr[item]] + `</button>`
-    menuHTMLString += `<button  type="button" class="btn btn-info" ><i class="material-icons" id = "`+ arr[item] +`">` + iconDict[arr[item]] + `</button>`
 
   }
 
