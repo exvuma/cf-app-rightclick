e413611a39d965fd087a0e2f104bf13c441449c3
Victoria Bernard <victoria@cloudflare.com>
2017-07-20 19:19:46 -0500

    script to storage broken

--- right-click.js
+++ right-click.js
@@ -7,19 +7,9 @@
   // chrome.tabs.query({'active': true}, function(tabs) {
   //   chrome.tabs.update(tabs[0].id, {url: newUrl});
   // });
-// var tri = window.Trianglify({
-//   width: 1000,
-//   height: 1000,
-//   // cell_size: reg.size,
-//   // x_colors: reg.colors,
-//   // variance: reg.variance / 100
-// })
 
 
 
-
-// svg2.style.background = "url('data:image/svg+xml;utf8," + svg.outerHTML + "') no-repeat"
-// svg2.style.backgroundSize = 'cover'
 
 
   function menuClicked (e) {
@@ -61,6 +51,7 @@
       'justifyRight': 'format_align_right'
 
     }
+    // Where I got icons from https://material.io/icons/#ic_format_strikethrough
     var iconDict = {
       'bold': '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"> \
               <path d="M15.6 10.79c.97-.67 1.65-1.77 1.65-2.79 0-2.26-1.75-4-4-4H7v14h7.04c2.09 0 3.71-1.7 3.71-3.79 0-1.52-.86-2.82-2.15-3.42zM10 6.5h3c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5h-3v-3zm3.5 9H10v-3h3.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5z"/>\
@@ -234,7 +225,7 @@
       // 'backgroundColor': '#FFFFFF',
       'modifiers': {
         'bold': true,
-        'backColor': false,
+        'backColor': true,
         'italic': true,
         'fontName': false,
         'strikeThrough': true,
@@ -258,6 +249,27 @@
       // openMenu('INSTALL')// passing in "INSTALL" tells the function to handle as an INSTALL
     }
   }
+  chrome.storage.sync.get(null, function(storage) { // gets all of the keys in storage
+      var options = { window: window }
+
+      for (var key in storage) {
+        options[key] = storage[key]
+      }
+
+
+      chrome.storage.onChanged.addListener(function(changes, namespace) {
+        for (key in changes) {
+          var storageChange = changes[key];
+          console.log('setting', key, storageChange.newValue)
+          Right_Click.options[key] = storageChange.newValue
+        }
+      })
+    })
+  } else {
+//     var sparkler = exports.sparkler = new CursorSparkler({
+//       window: window
+//       )}
+    }
   // This code ensures that the app doesn't run before the page is loaded.
   if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', updateFormats)
